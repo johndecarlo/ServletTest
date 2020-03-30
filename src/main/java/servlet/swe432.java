30d689238248e31c3ba450cecd0324230cc10263
John DeCarlo <jdecarlo841@gmail.com>
2020-03-30 17:51:18 -0400

    Adding .css file

--- src/main/java/servlet/swe432.java
+++ src/main/java/servlet/swe432.java
@@ -34,7 +34,7 @@ public class swe432 extends HttpServlet {
 		out.println("");
 		out.println("<head>");
 		out.println("<title>GMU Gym Review</title>");
-		out.println(" <link rel=\"stylesheet\" type=\"text/css\" href=\"http://mason.gmu.edu/~jdecarl/swe432.css \">");
+		out.println(" <link rel=\"stylesheet\" type=\"text/css\" href=\"" + "http://mason.gmu.edu/~jdecarl/swe432.css" + "\">");
 		out.println("</head>");
 		out.println("");
 	}
