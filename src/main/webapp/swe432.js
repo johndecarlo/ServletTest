870339bf1425fe6107ff65fdd7bdd494ba4e921b
John DeCarlo <jdecarlo841@gmail.com>
2020-03-30 23:56:35 -0400

    Implement .css

--- src/main/webapp/swe432.js
+++ src/main/webapp/swe432.js
@@ -1,6 +1,5 @@
 function checkGym (form) {
 	if(!(document.getElementById('afc').checked) && !(document.getElementById('rac').checked) && !(document.getElementById('skyline').checked)) {
-		alert ("Please select a gym.");
 		return false;
 	}
 	return true;
@@ -9,7 +8,6 @@ function checkGym (form) {
 function checkRating (form) {
 	if(!(document.getElementById('very_bad').checked) && !(document.getElementById('bad').checked) && !(document.getElementById('okay').checked)
 		&& !(document.getElementById('good').checked) && !(document.getElementById('very_good').checked)) {
-		alert ("Please give a rating of your experience.");
 		return false;
 	}
 	return true;
@@ -17,13 +15,18 @@ function checkRating (form) {
 
 function checkExercise (form) {
 	if(!(form.cardio.checked) && !(form.chest.checked) && !(form.back.checked) && !(form.arms.checked) && !(form.legs.checked) && !(form.core.checked) && !(form.basketball.checked) && !(form.swimming.checked) && !(form.other.checked)) {
-		alert ("Please select at least one exercise you did.");
 		return false;
 	}
 	return true;
 }
 		
 function submitForm (form) {
-	if(checkGym(form)) && checkExercise(form) && checkExercise(form))
+	if(checkGym(form))
+		alert ("Please select a gym.");
+	else if(checkExercise(form))
+		alert ("Please select at least one exercise you did.");
+	else if(checkRatintg(form))
+		alert ("Please select a rating.");
+	else
 		document.theForm.submit();
 }
