c2efd33a5eaecf649c71f76ed31292bdad68bb56
John DeCarlo <jdecarlo841@gmail.com>
2020-04-27 12:49:21 -0400

    Implemented workout list

--- src/main/java/servlet/swe432.java
+++ src/main/java/servlet/swe432.java
@@ -266,7 +266,7 @@ public class swe432 extends HttpServlet {
 		 String gym = request.getParameter(Data.GYM.name());
 		 String experience = request.getParameter(Data.EXPERIENCE.name());
 		 String[] workout = request.getParameterValues(Data.WORKOUT.name());
-		 Long time = session.getLastAccessedTime()
+		 Long time = session.getLastAccessedTime();
 		 String result = "";
 		 if(workout != null) {
 			 if(workout.length == 1)
